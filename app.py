b5061be2a9dad27f676d4f96a14cd70a73e3aac5
Priyankdimri <114757150+Priyankdimri@users.noreply.github.com>
2024-07-11 14:11:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ KNN=pickle.load(pickel_open)
 #     prediction=KNN.predict([[gender,age,hypertension,heart_disease,bmi,HbA1c_level,blood_glucose_level]])
 #     return prediction
 
-# def main():
-#     st.title("Welcome To Diabities prediction ")
-#     gender=st.text_input("gender","Type here")
-#     age=st.text_input("age","Type here")
-#     hypertension=st.text_input("hypertension","Type here")
-#     heart_disease=st.text_input("heart_disease","Type here")
-#     bmi=st.text_input("bmi","Type here")
-#     HbA1c_level=st.text_input("HbA1c_level","Type here")
-#     blood_glucose_level=st.text_input("blood_glucose_level","Type here")
-#     result=""
-#     if st.button("predict"):
-#         result=predict_note(gender,age,hypertension,heart_disease,bmi,HbA1c_level,blood_glucose_level)
-#     st.success(f"The Prediction is {result}")
+def main():
+    st.title("Welcome To Diabities prediction ")
+    gender=st.number_input("gender","Type here")
+    age=st.number_input("age","Type here")
+    hypertension=st.number_input("hypertension","Type here")
+    heart_disease=st.number_input("heart_disease","Type here")
+    bmi=st.number_input("bmi","Type here")
+    HbA1c_level=st.number_input("HbA1c_level","Type here")
+    blood_glucose_level=st.number_input("blood_glucose_level","Type here")
+    result=""
+    if st.button("predict"):
+        result=predict_note(gender,age,hypertension,heart_disease,bmi,HbA1c_level,blood_glucose_level)
+    st.success(f"The Prediction is {result}")
         
     
 
